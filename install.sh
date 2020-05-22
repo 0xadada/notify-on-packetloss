63c10ff6b18aa5819b3f0206aa5773fdb039805a
0xADADA <0xadada.pub@0xadada.pub>
2020-05-22 11:35:47 -0400

    shellcheck

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ launchctl load \
   "/Users/${USER}/Library/LaunchAgents/pub.0xadada.notify-on-packetloss.plist"
 result=$?
 
-if (( $result != 0 )); then
+if (( result != 0 )); then
   echo "Failed to install with code: ${result}"
 else
   pid=$(launchctl list | rg 'pub.0xadada.notify-on-packetloss' | awk '{print $1}')
